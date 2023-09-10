22d9cb11b0cddc733459e8c90830221f78016a87
bhoju <bhojrajmishra4@gmail.com>
2023-09-10 23:00:49 +0545

    edit button on single page

--- app.js
+++ app.js
@@ -1,28 +1,84 @@
-const express = require("express")// requiring express package
-const app = express()//storing it in app, app vaneko variable throughout the project use garxam
-//ma ejs use garna aateko xu ,, kk chayenxa ho env set garxa
-app.set("view engine","ejs")
+const express = require('express')
+const { blogs } = require('./model/index')
+const app = express()
+
+
+
+// database connection 
+require("./model/index")
+
+// telling the nodejs to set view-engine to ejs
+app.set('view engine','ejs')
+
+
+// form bata data aairaxa parse gara or handle gar vaneko ho
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+// allBlog
+app.get("/",async (req,res)=>{
+    //blogs vanney table bata vayejati sabai data dey vaneko 
+    const allBlogs = await blogs.findAll() 
 
-app.get('/',(req,res)=>{
-    //views vitra ko file render garxa
-    res.render("blogs")
+    // blogs vanney key/name ma allBlogs/data pass gareko ejs file lai
+    res.render('blogs',{blogs:allBlogs})
 })
 
-app.get('/createblogs',(req,res)=>{
+//createBlog
+app.get("/createblog",(req,res)=>{
     res.render("createblogs")
 })
 
-//createblog 
-
-app.post("/createblogs",(req,res)=>{
-    console.log(req.body)
-    res.send("form send successfuly")
+//createBlog Post
+app.post("/createblog",async (req,res)=>{
+    
+        // second approach
+        // const {title,description,subtitle} = req.body
+    // first approach
+    const title = req.body.title
+    const description  = req.body.description
+    const subTitle = req.body.subtitle
+   
+    // database ma halnu paryo , database sanaga kehi operation await halnu parney hunchha 
+    // agadi , await halepaxi mathi async halnu parney hunchha 
+    await blogs.create({
+        title : title,
+        subTitle:subTitle,
+        description : description
+    })
+    res.redirect("/")
 })
 
-// port number -> room number for the 
+// single blog page 
+app.get("/single/:id",async(req,res)=>{
+const id = req.params.id
+// second approach
+// const {id} = req.params 
+// id ko data magnu/find garnu paryo hamro table bata
+const blog =  await blogs.findAll({
+    where : {
+        id : id
+    }
+})
+// second finding approach
+// const blog = await blogs.findByPk(id)
+
+    res.render("singleBlog",{blog:blog})
+})
+
+// delete page 
+app.get("/delete/:id",async (req,res)=>{
+    const id = req.params.id
+    // blogs vanney table bata tyo id ko delete gar vaneko yaha
+     await blogs.destroy({
+        where : {
+            id : id
+        }
+    })
+   res.redirect("/")
+})
+
+
 app.listen(3000,()=>{
-    console.log("Node js project has started on 3000")
+    console.log("NodeJs project has started at port 3000")
 })
---
+++ model/index.js
@@ -0,0 +1,37 @@
+const dbConfig = require("../config/dbconfig");
+const { Sequelize, DataTypes } = require("sequelize");
+//la sequelize yo config lag 
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("CONNECTED!!");
+  })
+  .catch((err) => {
+    console.log("Error" + err);
+  });
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+//importing model files
+db.blogs = require("./blogModel.js")(sequelize, DataTypes);
+// db.reviews = require("./reviewModel.js")(sequelize, DataTypes);
+
+db.sequelize.sync({ force: false }).then(() => {
+  console.log("yes re-sync done");
+});
+
+module.exports = db;
